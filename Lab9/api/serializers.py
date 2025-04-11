e7ce09317f68c3e076d27360fb5b1b105c20599d
Yerlanuly Adiyet <erlanulyadiet0@gmail.com>
2025-04-11 18:27:35 +0500

    Initial commit with Lab9

---
+++ Lab9/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from api.models import Company, Vacancy
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('id', 'name', 'description', 'city', 'address')
+
+class VacancySerializer(serializers.ModelSerializer):
+    company_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Vacancy
+        fields = ('id', 'name', 'description', 'salary', 'company', 'company_id')
+        read_only_fields = ('company',) 
